c9d7eec17fb122e0e9a4af2b9650366f5599059a
Kelvin Chan <33172485+MyAngelNeptune@users.noreply.github.com>
2018-08-13 04:13:18 -0700

    Successfully grabbed image link from starting site
    Still can't download though

--- getAnimePics/spiders/coverspider.py
+++ getAnimePics/spiders/coverspider.py
@@ -5,22 +5,22 @@ import scrapy
 
 class CoverSpider(scrapy.Spider):
     name = "pyimagesearch-cover-spider"
+    allowed_domains = ['gelbooru.com/']
     start_urls = ["https://gelbooru.com/index.php?page=post&s=list&tags=solo+tomoe_gozen_%28fate%2Fgrand_order%29+rating%3Asafe&pid=84"] 
 
     def parse(self, response):
         #For every thumbnail, extract the image link from under the span a attribute
-        for url in response.xpath(""):
-            yield scrapy.Request(url.css("span a::attr(href)").extract_first(), self.parse_images)
-        #url = response.css("span a::attr(href)").extract()
-    
-    def parse_images(self, response):
-        url = response.css("img::atr(src)").extract()
+        #for url in response.xpath("//div[@class='thumbnail-preview']"):
+        #    yield scrapy.Request(response.css("span a::attr(href)").extract_first(), self.parse_images)
+        url = response.css("span a::attr(href)").extract()
         for item in zip(url):
-            scraped_info = {
-                'image_urls' : [item[0]]
-            }
-        
-        yield scraped_info
+            imageLink = ''.join(item)
+            imageLink = "https:" + imageLink
+            yield scrapy.Request(imageLink, self.parse_images)
+    def parse_images(self, response):
+        imageUrl = response.css("img::attr(src)").extract()
+        yield GetanimepicsItem(image_urls=[imageUrl])
+
 
             
 
